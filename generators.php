7f54795b1688acce3d782ece6d6c6e49a157c91a
hanshenrik <divinity76@gmail.com>
2017-06-22 15:21:06 +0200

    (PDO) DBToClasses

--- generators.php
+++ generators.php
@@ -1,6 +1,40 @@
 <?php
 declare(strict_types = 1);
 require_once ('common.inc.php');
+function DBToClasses(\PDO $db_handle, string $dbname): array {
+	/** @var PhpClass[] $classes */
+	$classes = array ();
+	$db = $db_handle;
+	unset ( $db_handle );
+	$tables = $db->query ( 'SHOW TABLES IN `' . $dbname . '`', PDO::FETCH_NUM );
+	// hhb_var_dump($tables->fetchAll(PDO::FETCH_ASSOC)) & die();
+	foreach ( $tables as $table ) {
+		$table = $table [0];
+		$class = new PhpClass ();
+		$classes [] = $class;
+		$class->name = ucfirst ( $table );
+		$cols = $db->query ( 'SHOW COLUMNS FROM `' . $table . '`' )->fetchAll ( PDO::FETCH_ASSOC );
+		foreach ( $cols as $col ) {
+			// var_dump ( $col ) & die ();
+			$member = new PhpClassMember ();
+			$class->members [] = $member;
+			$member->name = $col ['Field'];
+			$member->type = guessPhpType ( $col ['Type'] );
+			if (! empty ( $member->type )) {
+				$tmp = filter_var ( $col ['Null'], FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE );
+				if ($tmp === true) {
+					$member->type .= '|null';
+				}
+				unset ( $tmp );
+			}
+		}
+	}
+	return $classes;
+}
+function objectToClasses($obj) {
+	$arr = json_decode ( json_encode ( $obj ), true );
+	return arrayToClasses ( $arr );
+}
 function arrayToClasses(array $arr, string $basename = '?'): array {
 	/** @var PhpClass[] $classes */
 	$classes = array ();
